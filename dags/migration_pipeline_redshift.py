67b7a898a18d0d8565e3f29e7544a9090cec56d4
Dong Yang Jin <dongyang@hubble.sg>
2019-09-11 14:54:55 +0800

    edited redshift pipeline

--- dags/migration_pipeline_redshift.py
+++ dags/migration_pipeline_redshift.py
@@ -3,10 +3,12 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-from modules.db_integration_to_rds.data_pipeline import periodic_data_transfer as centralizedrds_pipeline_daily
+from modules.db_integration_to_rds.data_pipeline import get_daily_timerange, parse_config, transfer_module
 
 from datetime import datetime
 from datetime import timedelta
+import json
+from collections import OrderedDict
 
 """
 This script would be used to orchestrate all activities in the k8 container 
@@ -44,14 +46,30 @@ with DAG(
         task_id = 'start_of_daily_pipeline'
     )
 
-   # ------------ REDSHIFT TASK ------------------------    
-    daily_rds_task = PythonOperator(
-        task_id = 'rds_redshift_pipeline',
-        python_callable = centralizedrds_pipeline_daily
-    )
-
     end_task = DummyOperator(
         task_id = 'end_daily_pipeline'
     )
 
-    start_task >> daily_rds_task >> end_task
+    # ------------ REDSHIFT TASK ------------------------
+    cfg = parse_config()
+    start_time, end_time = get_daily_timerange(cfg['pivot_time'], cfg['timezone'])
+    
+    with open(cfg['module_table_meta']) as f:
+        module_table = json.load(f, object_pairs_hook=OrderedDict)
+    
+    for module in module_table:
+        if module in cfg['unused_modules']:
+            continue  
+        
+        task_id = "%s_%s" % ("migration_", module)
+
+        daily_redshift_task = PythonOperator(
+            task_id = task_id,
+            python_callable = transfer_module,
+            op_kwargs={
+                'module': module,
+                'start_time': start_time,
+                'end_time': end_time
+            }
+        )
+        start_task >> daily_redshift_task >> end_task
