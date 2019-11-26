429d8d4ff5ba52e62bcc1c9361ea38566e650c9b
Dong Yang Jin <dongyang@hubble.sg>
2019-11-26 22:07:36 +0800

    update script to cater for different tags for multiple platform versions in one docker image system

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -1,18 +1,33 @@
 
-# There is a need to reformat this properly to get the it automated.
+# There is a need to reformat this properly to get the it automated. (THIS IS SOLELY FOR THE HUBBLE PLATFORM.)
+# Change the category tag if needed to separate another airflow repo for another usage.
+# Main Airflow has no category tag.
 # 1: tag it incrementally
 # 2: automatically find the docker image with the latest built tag
-# 3: push the correct tag into the hubble docker repo
+# 3: push the correct tag into the hubble docker repo, but for the DELPHI platform
 
 # Local Tags
-AIRFLOW_LOCAL_DOCKER='hubble/docker-airflow'
-AIRFLOW_LOCAL_TAG='latest'
-
+AIRFLOW_LOCAL_DOCKER='company/docker-airflow'
+AIRFLOW_LOCAL_TAG='platforma-latest'
 # AWS Tags
-AIRFLOW_ECR_DOCKER='175416825336.dkr.ecr.ap-southeast-1.amazonaws.com/airflow'
+AIRFLOW_ECR_DOCKER='example.dkr.ecr.region.amazonaws.com/airflow'
+AIRFLOW_CATEGORY='platforma'
+
+
+# Set current tag based on the needed platform
+# Change the filter at 'contains("delphi-") to the airflow category you need instead
 AIRFLOW_CURRENT_ECR_TAG=\
-$(aws ecr describe-images --output json --repository-name airflow \
---query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' | jq . --raw-output)
+$(aws ecr describe-images --output json --repository-name airflow --image-ids imageTag=$AIRFLOW_CATEGORY \
+--query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[*]' | jq -c '[.[] | select(contains("platforma-"))] | sort_by(.) | .[-1]' --raw-output)
+
+# If first time run, this tag will be empty, thus set it to default
+if [ -z "$AIRFLOW_CURRENT_ECR_TAG" ]
+then 
+    AIRFLOW_CURRENT_ECR_TAG=$AIRFLOW_CATEGORY'-1.0.0'
+    echo "ECR Tag not set. Set as $AIRFLOW_CURRENT_ECR_TAG for now."
+fi
+
+# Next tag increment
 AIRFLOW_NEXT_ECR_TAG=\
 $(echo $AIRFLOW_CURRENT_ECR_TAG | awk -F. -v OFS=. 'NF==1{print ++$NF}; \
 NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
@@ -20,22 +35,39 @@ NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($
 echo "ECR Docker to push to: $AIRFLOW_ECR_DOCKER"
 echo "ECR Tag currently on: $AIRFLOW_CURRENT_ECR_TAG"
 echo "ECR Tag going next: $AIRFLOW_NEXT_ECR_TAG"
-echo "\n"
+echo ""
 
 case "$1" in
+    # Pushes to the next increment tag - the code takes in only one digit separate by dots. e.g. 1.9.9 --> 2.0.0
     next)
         echo "Building Next Tag: $AIRFLOW_ECR_DOCKER:$AIRFLOW_NEXT_ECR_TAG"
         docker build --rm -t ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} .
         $(aws ecr get-login --no-include-email --region ap-southeast-1)
-        docker tag hubble/docker-airflow:latest ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_NEXT_ECR_TAG}
+        docker tag ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_NEXT_ECR_TAG}
         docker push ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_NEXT_ECR_TAG}
+        docker tag ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CATEGORY}
+        docker push ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CATEGORY}
         ;;
-    current)
+
+    # This does not work for me as it requires permission to delete tagged images in AWS ECR Docker.
+    current) 
         echo "Building Current Tag: $AIRFLOW_ECR_DOCKER:$AIRFLOW_CURRENT_ECR_TAG"
         docker build --rm -t ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} .
         $(aws ecr get-login --no-include-email --region ap-southeast-1)
-        docker tag hubble/docker-airflow:latest ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CURRENT_ECR_TAG}
         aws ecr batch-delete-image --repository-name ${AIRFLOW_ECR_DOCKER} --image-ids imageTag=${AIRFLOW_CURRENT_ECR_TAG}
+        docker tag ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CURRENT_ECR_TAG}
         docker push ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CURRENT_ECR_TAG}
+        docker tag ${AIRFLOW_LOCAL_DOCKER}:${AIRFLOW_LOCAL_TAG} ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CATEGORY}
+        docker push ${AIRFLOW_ECR_DOCKER}:${AIRFLOW_CATEGORY}
         ;;
+
+    # Push the updated files to git as well
+    git)
+        echo "Pushing to git"
+        read -e -p "Enter Your Commit Message: "  message
+        git add .
+        git commit -m "$message"
+        git push
 esac
+
+
